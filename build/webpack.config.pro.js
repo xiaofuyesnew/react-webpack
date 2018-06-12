45b0f1969c4bd25a711115060d66ab51f3146a8f
xiaofuyesnew <xiaofuyesnew@qq.com>
2018-06-12 08:44:23 +0800

    提交

---
+++ build/webpack.config.pro.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: path.join(__dirname, '../src/app.jsx')
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js|\.jsx$/,
+      use: ['babel-loader?cacheDirectory=true'],
+      include: path.join(__dirname, '../src')
+    }]
+  }
+}
